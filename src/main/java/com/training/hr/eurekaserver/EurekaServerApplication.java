83cad4b99fa0f18971896b2030794b9766ad4013
DaveDevOps+1-at-909034962694 <dmora.e99sgmail.comdmora.e99sgmail.com>
2024-06-04 19:12:21 -0600

    Eureka Server

---
+++ src/main/java/com/training/hr/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.training.hr.eurekaserver;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
